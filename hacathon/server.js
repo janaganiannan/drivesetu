9c87ad58e18f963b070a6f60e3664ca762920e54
Phoenixlegends003 <annanjanagani003@gmail.com>
2026-08-14 02:31:22 +0530

    fix: Disable server/browser caching headers and bump script version to v=500 for instant live deployment updates

--- hacathon/server.js
+++ hacathon/server.js
@@ -5,16 +5,23 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Disable caching headers for instant client updates
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    next();
+});
+
 // Determine static root directory (hacathon if present, else current dir)
 const staticDir = fs.existsSync(path.join(__dirname, 'hacathon')) 
     ? path.join(__dirname, 'hacathon') 
     : __dirname;
 
-app.use(express.static(staticDir));
-app.use(express.static(__dirname));
+app.use(express.static(staticDir, { etag: false, lastModified: false }));
+app.use(express.static(__dirname, { etag: false, lastModified: false }));
 
 // Serve index.html for all routes (Single Page Application support)
 app.get('*', (req, res) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
     if (fs.existsSync(path.join(staticDir, 'index.html'))) {
         res.sendFile(path.join(staticDir, 'index.html'));
     } else {
